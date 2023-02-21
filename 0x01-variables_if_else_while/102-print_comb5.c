bf4d6ed1a68d20b4fd0120ab8dc15308ef28ddb8
NnamdiCJ <great.nnamdi@yahoo.com>
2023-02-20 16:11:20 -0800

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (k = 48; k <= 57; k++)
 {
 for (j = 48; j <= 57; j++)
 {
-if (((k + j) > (c + i) && k >== c) || c < k)
+if (((k + j) > (c + i) && k >= c) || c < k)
 {
 putchar(c);
 putchar(i);
@@ -36,7 +36,7 @@ putchar(' ');
 }
 }
 }
-} n;
+}
 }
 }
 putchar('\n');
