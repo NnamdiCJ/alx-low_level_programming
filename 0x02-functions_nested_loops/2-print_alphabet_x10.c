a96b6c1fdda25e1e993605a750b7fec6e90b87a0
NnamdiCJ <great.nnamdi@yahoo.com>
2023-02-22 11:34:36 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,8 @@
  * print_alphabet_10 - prints all alphabets in lowercase 10 times.
  * Return: On success 1.
  */
-void print_alphabet_x10(void);
-()
+void print_alphabet_x10(void);(
+
 {
 char i, k;
 
