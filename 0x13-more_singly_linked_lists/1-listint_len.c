db8264f4ca36e9aac700bc8cb469eadb5e8ac5ca
NnamdiCJ <great.nnamdi@yahoo.com>
2023-04-05 11:25:12 -0700

    task34

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,21 +7,21 @@
  **/
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes;
+size_t nodes;
 
-	/* Initialize nodes variable to 0 */
-	nodes = 0;
+/* Initialize nodes variable to 0 */
+nodes = 0;
 
-	/* Loop over each node in the list */
+/* Loop over each node in the list */
 while (h)
-	{
-		/* Increment nodes variable by 1 for each node */
-		nodes++;
+{
+/* Increment nodes variable by 1 for each node */
+nodes++;
 
-		/* Move to the next node by following the next pointer */
-		h = h->next;
-	}
+/* Move to the next node by following the next pointer */
+h = h->next;
+}
 
-	/* Return the final value of nodes */
+/* Return the final value of nodes */
 return (nodes);
 }
