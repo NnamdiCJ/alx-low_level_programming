3a3263c179267ee7eb48ff0c50bec93e9d650be1
NnamdiCJ <great.nnamdi@yahoo.com>
2023-04-10 15:41:36 -0700

    taskgo

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,13 +2,13 @@
 /**
  * flip_bits - counts the number of bits needed to be
  * flipped to get from one number to another
- * @t: the number
- * @m: the number to flip t to
- * Return: the necessary number of bits to flip to get t to m.
+ * @n: the number
+ * @m: the number to flip n to
+ * Return: the necessary number of bits to flip to get n to m.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = t ^ m, bits = 0;
+	unsigned long int xor = n ^ m, bits = 0;
 
 	while (xor > 0)
 	{bits += (xor & 1);
